85eecc5fb026244fc1e7b37e87f0ec0612817b8f
Jared Recomendable <41513577+JDCRecomendable@users.noreply.github.com>
2022-07-26 13:29:54 +1200

    Support finding min and avg for array

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const arrayStats = (nums) => ({
     max: Math.max(...nums),
+    average: nums.length ? nums.reduce((a, b) => a + b) / nums.length : 0,
+    min: Math.min(...nums),
 });
 
 const stats = arrayStats([-4, 9, 5]);
