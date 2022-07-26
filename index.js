c42132ce9fee9f532a5e889f74dfca260c3d869f
Jared Recomendable <41513577+JDCRecomendable@users.noreply.github.com>
2022-07-26 13:29:54 +1200

    Also support avg of array (#2)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const arrayStats = (nums) => ({
     max: Math.max(...nums),
+    average: nums.length ? nums.reduce((a, b) => a + b) / nums.length : 0,
 });
 
 const stats = arrayStats([-4, 9, 5]);
